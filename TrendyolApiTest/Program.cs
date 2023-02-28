ca8dc61c42090d7a002d4173ce2a65852088279a
Kadir Hali <kadirhali07@gmail.com>
2023-02-28 15:38:56 +0300

    28.02.2023

--- TrendyolApiTest/Program.cs
+++ TrendyolApiTest/Program.cs
@@ -1,24 +1,10 @@
 ﻿using System;
 using System.Text;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.IO;
-using System.Configuration;
-using System.Security.Cryptography.X509Certificates;
-using static TrendyolApiTest.Program;
-
 namespace TrendyolApiTest
 {
-    public class Product
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Category { get; set; }
-    }
     internal class Program
     {
         static void Main(string[] args)
@@ -26,50 +12,28 @@ namespace TrendyolApiTest
             RunAsync().Wait();
             Console.ReadLine();
         }
-
-        public class User
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Username { get; set; }
-            public string Email { get; set; }
-        }
-
         static async Task RunAsync()
         {
-            string username = "hdAysqagvjVihbfsvZt6";
-            string password = "idUv2kljvSa9qeLSj4gK";
-            string supplierID = "330419";
+            string username = AccountInfo.username;
+            string password = AccountInfo.password;
+            string supplierID = AccountInfo.supplierID;
 
             using (var client = new HttpClient())
             {
                 // Send HTTP requests
-                client.BaseAddress = new Uri("https://api.trendyol.com/sapigw/");
+                client.BaseAddress = new Uri(URLs.BaseURL);
                 client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(username + ":" + password)));
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(DataFormatType.Json));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(AuthenticationType.Basic, Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(username + ":" + password)));
 
-
-                await GetAllProducts(supplierID, client);
+                //await GetAllProducts(supplierID, client);
                 //await GetBrands(client);
-                //await GetReturnAdress(supplierID, client);
+                await GetReturnAdress(supplierID, client);
             }
         }
-
         private static async Task GetReturnAdress(string supplierID, HttpClient client)
         {
-            HttpResponseMessage response3 = await client.GetAsync("suppliers/" + supplierID + "/addresses");
-            if (response3.IsSuccessStatusCode)
-            {
-                Console.WriteLine(response3.ToString());
-                string product = await response3.Content.ReadAsStringAsync();
-                Console.WriteLine(await response3.Content.ReadAsStringAsync());
-            }
-        }
-
-        private static async Task GetAllProducts(string supplierID, HttpClient client)
-        {
-            HttpResponseMessage response = await client.GetAsync("suppliers/" + supplierID + "/products?approved=true");
+            HttpResponseMessage response = await client.GetAsync(URLs.Suppliers + supplierID + URLs.Addresses);
             if (response.IsSuccessStatusCode)
             {
                 Console.WriteLine(response.ToString());
@@ -77,15 +41,24 @@ namespace TrendyolApiTest
                 Console.WriteLine(await response.Content.ReadAsStringAsync());
             }
         }
-
+        private static async Task GetAllProducts(string supplierID, HttpClient client)
+        {
+            HttpResponseMessage response = await client.GetAsync(URLs.Suppliers + supplierID + URLs.AllProducts);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.ToString());
+                string product = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
         private static async Task GetBrands(HttpClient client)
         {
-            HttpResponseMessage response2 = await client.GetAsync("brands");
-            if (response2.IsSuccessStatusCode)
+            HttpResponseMessage response = await client.GetAsync(URLs.Brands);
+            if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine(response2.ToString());
-                string product = await response2.Content.ReadAsStringAsync();
-                Console.WriteLine(await response2.Content.ReadAsStringAsync());
+                Console.WriteLine(response.ToString());
+                string product = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
             }
         }
     }  
---
+++ TrendyolApiTest/Strings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendyolApiTest
+{
+    public static class AccountInfo
+    {
+        public static string username = "hdAysqagvjVihbfsvZt6";
+        public static string password = "idUv2kljvSa9qeLSj4gK";
+        public static string supplierID = "330419";
+    }
+    public static class DataFormatType
+    {
+        public static string Json = "application/json";
+    }
+    public static class AuthenticationType
+    {
+        public static string Basic = "Basic";
+    }
+
+    public static class URLs
+    {
+        public static string BaseURL = "https://api.trendyol.com/sapigw/";
+        public static string Suppliers = "suppliers/";
+        public static string Addresses = "/addresses";
+        public static string AllProducts = "/products?approved=true";
+        public static string Brands = "brands";
+    }
+
+}
